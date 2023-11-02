e1201b20b814f0b840686c55c3e17f18a794a60f
e-d-p-9 <140538808+e-d-p-9@users.noreply.github.com>
2023-11-02 09:45:01 -0500

    add length check assertion

--- Ch4/simple_plot.py
+++ Ch4/simple_plot.py
@@ -10,5 +10,8 @@ num_points = 5
 x_min, x_max = 0, 4
 x_values = np.linspace(x_min, x_max, num_points)
 y_values = x_values**2
+# y_values = y_values[2:]
+assert len(x_values) == len(y_values), \
+    "Length-mismatch: {:d} versus {:d}".format(len(x_values), len(y_values))
 plt.plot(x_values, y_values)
 plt.show()
