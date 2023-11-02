2027e5f6832e9e4f9d3eade5774c13ca6228f104
e-d-p-9 <140538808+e-d-p-9@users.noreply.github.com>
2023-11-02 09:13:14 -0500

    update save load script to load data from files

--- Ch4/save_load.py
+++ Ch4/save_load.py
@@ -10,9 +10,20 @@ import numpy as np
 x = np.linspace(0, 1, 1001)
 y = 3*np.sin(x)**3 - np.sin(x)
 
+# save data to files
 np.save('x_values', x)
 np.save('y_values', y)
 np.savetxt('x_values.dat', x)
 np.savetxt('y_values.dat', y)
 np.savez('xy_values', x_vals=x, y_vals=y)
+
+# load data from files
+x2 = np.load('x_values.npy')
+y2 = np.loadtxt('y_values.dat')
+w = np.load('xy_values.npz')
+
+print(x2 == x)
+print(y2 == y)
+print(w['x_vals'] == x)
+print(w['y_vals'] == y)
 
